dbad2f28c3dde00f284190e7375d7fec825c0ec6
Alexei Golubchikov <a.g.86@yandex.ru>
2023-05-18 00:02:41 +0500

    feat: добавить форму добавления карточки

--- scripts/index.js
+++ scripts/index.js
@@ -1,32 +1,57 @@
 const profileButtonEdit = document.querySelector('.profile__edit-button');
+const profileButtonAdd = document.querySelector('.profile__add-button');
 const popupButtonClose = document.querySelector('.popup__close');
-const popup = document.querySelector('.popup');
-const formName = document.querySelector('.form__field_type_name');
+const popupCardButtonClose = document.querySelector('.popup__close_type_card');
+const popupEdit = document.querySelector('.popup_type_edit');
+const popupAdd = document.querySelector('.popup_type_add');
 const profileName = document.querySelector('.profile__name');
-const formJob = document.querySelector('.form__field_type_job');
 const profileJob = document.querySelector('.profile__job');
+const formName = document.querySelector('.form__field_type_name');
+const formJob = document.querySelector('.form__field_type_job');
+const formTitle = document.querySelector('.form__field_type_title');
+const formLink = document.querySelector('.form__field_type_link');
 const formElement = document.querySelector('.form');
+const elements = document.querySelector('.elements');
 
-function showPopupContainer() {
-    popup.classList.add('popup_opened');
+//функция открытия формы редактирования профиля
+function showPopupEditContainer() {
+    popupEdit.classList.add('popup_opened');
     formName.value = profileName.textContent;
     formJob.value = profileJob.textContent;
 }
-profileButtonEdit.addEventListener('click', showPopupContainer);
+//метод открытия формы редактирования профиля
+profileButtonEdit.addEventListener('click', showPopupEditContainer);
 
-function closePopupContainer() {
-    popup.classList.remove('popup_opened');
+//функция открытия формы добавления карточки
+function showPopupAddCard() {
+  popupAdd.classList.add('popup_opened');
 }
+//метод открытия формы добавления карточки
+profileButtonAdd.addEventListener('click', showPopupAddCard);
+
+//функция закрытия формы редактирования профиля
+function closePopupContainer() {
+    popupEdit.classList.remove('popup_opened');
+}
+//метод закрытия формы редактирования профиля
 popupButtonClose.addEventListener('click', closePopupContainer);
 
+//функция закрытия формы добавления карточки
+function closePopupAddCard() {
+  popupAdd.classList.remove('popup_opened');
+}
+//метод закрытия формы добавления карточки
+popupCardButtonClose.addEventListener('click', closePopupAddCard);
+
+//функция сохранения введенных данных в форму редактирования профиля
 function saveNewDataProfile(evt) {
     evt.preventDefault();
     profileName.textContent = formName.value;
     profileJob.textContent = formJob.value;
     closePopupContainer();
 }
+//метод сохранения введенных данных в форму редактирования профиля
 formElement.addEventListener('submit', saveNewDataProfile);
-
 
 
 
@@ -58,14 +83,13 @@ const initialCards = [
   ];
   
 
-function addImage(element) {
-    const elements = document.querySelector('.elements');
+function addCards(element) {
     const elementTemplate = document.querySelector('#element-template').content;
-    const imageElement = elementTemplate.querySelector('.element').cloneNode(true);
-    imageElement.querySelector('.element__title').textContent = element.name;
-    imageElement.querySelector('.element__image').src = element.link;
-    imageElement.querySelector('.element__image').alt = element.name;
-    elements.append(imageElement);
+    const cardElement = elementTemplate.querySelector('.element').cloneNode(true);
+    cardElement.querySelector('.element__title').textContent = element.name;
+    cardElement.querySelector('.element__image').src = element.link;
+    cardElement.querySelector('.element__image').alt = element.name;
+    elements.append(cardElement);
 }
 
-initialCards.forEach(addImage);
+initialCards.forEach(addCards);
