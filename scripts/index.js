81326eb612707ee3962838dc8d8eaf8eded56726
Alexei Golubchikov <a.g.86@yandex.ru>
2023-06-04 19:33:06 +0500

    feat: добавить закрытие модального окна при клике на оверлей

--- scripts/index.js
+++ scripts/index.js
@@ -9,12 +9,13 @@ const formEditName = document.querySelector('.form__field_type_name');
 const formEditJob = document.querySelector('.form__field_type_job');
 const formAddTitleCard = document.querySelector('.form__field_type_title');
 const formAddLinkCard = document.querySelector('.form__field_type_link');
-const formEditProfile = document.querySelector('.form');
+const formEditProfile = document.querySelector('.form_type_edit');
 const formAddCard = document.querySelector('.form_type_add');
 const cards = document.querySelector('.elements');
 const captionPopupImage = popupShowImage.querySelector('.popup__caption');
 const popupCard = popupShowImage.querySelector('.popup__image');
 const elementTemplate = document.querySelector('#element-template').content;
+const formPopup = document.querySelector('.form');
 
 //функция открытия модального окна
 function openPopup(popup) {
@@ -31,13 +32,24 @@ popupButtonClose.forEach((btn) => {
     const popupClosest = btn.closest('.popup');
     closePopup(popupClosest);
   });
-}); 
+});
+
+//закрыте окна при клике на оверлей
+const popup = document.querySelectorAll('.popup');
+popup.forEach((popup) => {
+  popup.addEventListener('click', (evt) => {
+    if (evt.target === evt.currentTarget) {
+      popup.classList.remove('popup_opened');
+    }
+  })
+});
+  
 
 //открытие формы редактирования профиля
 buttonEditProfile.addEventListener('click', () => {
   openPopup(popupEditProfile);
-  formEditName.value = profileName.textContent;
-  formEditJob.value = profileJob.textContent;
+  //formEditName.value = profileName.textContent;
+  //formEditJob.value = profileJob.textContent;
 });
 
 //открытие формы добавления карточки
@@ -51,6 +63,7 @@ function saveNewDataProfile(evt) {
     profileName.textContent = formEditName.value;
     profileJob.textContent = formEditJob.value;
     closePopup(popupEditProfile);
+    formEditProfile.reset();
 };
 formEditProfile.addEventListener('submit', saveNewDataProfile);
 
@@ -95,3 +108,64 @@ function addCard(evt) {
   formAddCard.reset();
 }
 formAddCard.addEventListener('submit', addCard);
+
+
+//валидация
+const hasInvalidInput = (inputList) => {
+  return inputList.some((inputElement) => {
+    return !inputElement.validity.valid;
+  })
+}; 
+const showInputError = (formPopup, formEditName, errorMessage) => {
+  const formEditNameError = formPopup.querySelector(`.${formEditName.id}-error`);
+  formEditName.classList.add('form__field_type_error');
+  formEditNameError.classList.add('input-error_active');
+  formEditNameError.textContent = errorMessage;
+};
+
+const hideInputError = (formPopup, formEditName) => {
+  const formEditNameError = formPopup.querySelector(`.${formEditName.id}-error`);
+  formEditName.classList.remove('form__field_type_error');
+  formEditNameError.classList.remove('input-error_active');
+  formEditNameError.textContent = '';
+};
+
+const isValid = (formPopup, formEditName) => {
+  if (!formEditName.validity.valid) {
+    showInputError(formPopup, formEditName, formEditName.validationMessage);
+  } else {
+    hideInputError(formPopup, formEditName);
+  }
+};
+
+const toggleButtonState = (inputList, buttonElement) => {
+  if (hasInvalidInput(inputList)) {
+    buttonElement.classList.add('form__button_inactive');
+  } else {
+    buttonElement.classList.remove('form__button_inactive');
+  }
+}; 
+
+const setEventListeners = (formPopup) => {
+  const buttonElement = formPopup.querySelector('.form__button');
+  const inputList = Array.from(formPopup.querySelectorAll('.form__field'));
+  inputList.forEach((inputElement) => {
+    inputElement.addEventListener('input', () => {
+      toggleButtonState(inputList, buttonElement);
+      isValid(formPopup, inputElement);
+    });
+  });
+  toggleButtonState(inputList, buttonElement);
+};
+
+const enableValidation = () => {
+  const formList = Array.from(document.querySelectorAll('.form'));
+  formList.forEach((formElement) => {
+    formElement.addEventListener('submit', (evt) => {
+      evt.preventDefault();
+    });
+    setEventListeners(formElement);
+  });
+};
+
+enableValidation();
